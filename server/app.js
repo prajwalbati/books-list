34f329b29a62a6bd8d5791d815772eeb3d0af94d
Prajwal Bati <prajwalbati@gmail.com>
2023-09-03 01:00:23 -0400

    setup express with graphql

---
+++ server/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const {graphqlHTTP} = require('express-graphql');
+
+const app = express();
+
+app.use('/graphql', graphqlHTTP({
+
+}));
+
+const PORT = 4000;
+app.listen(PORT, () => {
+    console.log(`Server listening in the port: ${PORT}`)
+});
