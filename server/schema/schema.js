76c862595455b299adb6aebe4be5e959a26d3f70
Prajwal Bati <prajwalbati@gmail.com>
2023-09-03 20:36:02 -0400

    add author type

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,13 +1,22 @@
 const graphql = require('graphql');
 
-const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+const { GraphQLObjectType, GraphQLSchema, GraphQLID, GraphQLInt, GraphQLString } = graphql;
 
 const BookType = new GraphQLObjectType({
     name: 'Book',
     fields: () => ({
-        id: { type: GraphQLString },
+        id: { type: GraphQLID },
         name: { type: GraphQLString },
         genre: { type: GraphQLString }
+    })
+});
+
+const AuthorType = new GraphQLObjectType({
+    name: 'Author',
+    fields: () => ({
+        id: { type: GraphQLID },
+        name: { type: GraphQLString },
+        age: { type: GraphQLInt }
     })
 });
 
@@ -16,10 +25,17 @@ const RootQuery = new GraphQLObjectType({
     fields: {
         book: {
             type: BookType,
-            args: { id: { type: GraphQLString } },
+            args: { id: { type: GraphQLID } },
             resolve(parent, args) {
                 // code to get data from data source
                 return {name: 'Sample', genre: 'test', id: '1'};
+            }
+        },
+        author: {
+            type: AuthorType,
+            args: { id: { type: GraphQLID }},
+            resolve(parent, args) {
+                return { id: '1', name: 'Prajwal Bati', age: '25'};
             }
         }
     }
